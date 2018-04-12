46d32ea02ac07f3a55b38c16509f7c1b2147d721
Majed Sief Al-Nasr <magedelmasry@outlook.com>
2018-04-12 14:20:28 +0200

    RE-FORMATE UserAuth.java
    Signed-off-by: Majed Sief Al-Nasr <magedelmasry@outlook.com>

--- app/src/main/java/csc/colleguescheduller/Models/FireBase/UserAuth.java
+++ app/src/main/java/csc/colleguescheduller/Models/FireBase/UserAuth.java
@@ -8,7 +8,6 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-
 public class UserAuth {
     private FirebaseAuth auth;
     private FirebaseUser user;
@@ -17,13 +16,15 @@ public class UserAuth {
     public UserAuth() {
         this.auth = FirebaseAuth.getInstance();
         this.user = auth.getCurrentUser();
-        this.message  = new String[1];
+        this.message = new String[1];
     }
 
-    public String getStatus(){
+    public String getStatus() {
         // return user login status
-        if (user != null) message[0] = "LOGGED_IN";
-        else message[0] = "LOGGED_OUT";
+        if (user != null)
+            message[0] = "LOGGED_IN";
+        else
+            message[0] = "LOGGED_OUT";
         return message[0];
     }
 
@@ -46,40 +47,40 @@ public class UserAuth {
     }
 
     public String updateDisplayName(String displayName) {
-        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
-                .setDisplayName(displayName)
+        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder().setDisplayName(displayName)
                 .build();
 
-        user.updateProfile(profileUpdates)
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        if (task.isSuccessful()) message[0] = "SUCCESS";
-                        else message[0] = "FAILED";
-                    }
-                });
+        user.updateProfile(profileUpdates).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful())
+                    message[0] = "SUCCESS";
+                else
+                    message[0] = "FAILED";
+            }
+        });
         return message[0];
     }
 
     public String updateEmail(String email, String password) {
-        user.updateEmail(email)
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        if (task.isSuccessful()) message[0] = "SUCCESS";
-                        else message[0] = "FAILED";
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
+        user.updateEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful())
+                    message[0] = "SUCCESS";
+                else
+                    message[0] = "FAILED";
+            }
+        }).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception e) {
                 if (e instanceof FirebaseAuthRecentLoginRequiredException)
                     message[0] = "ERROR_RE-AUTHENTICATION_NEEDED";
-				else if (e instanceof FirebaseAuthUserCollisionException ) {
-					
-                    String errorCode =
-                            ((FirebaseAuthUserCollisionException ) e).getErrorCode();
+                else if (e instanceof FirebaseAuthUserCollisionException) {
+
+                    String errorCode = ((FirebaseAuthUserCollisionException) e).getErrorCode();
                     message[0] = errorCode;
-				}
+                }
             }
         });
 
@@ -87,14 +88,15 @@ public class UserAuth {
     }
 
     public String updatePassword(String oldPassword, String newPassword) {
-        user.updatePassword(newPassword)
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        if (task.isSuccessful()) message[0] = "SUCCESS";
-                        else message[0] = "FAILED";
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
+        user.updatePassword(newPassword).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful())
+                    message[0] = "SUCCESS";
+                else
+                    message[0] = "FAILED";
+            }
+        }).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception e) {
                 if (e instanceof FirebaseAuthRecentLoginRequiredException)
@@ -107,75 +109,75 @@ public class UserAuth {
         return message[0];
     }
 
-    public String resetPassword(String email){
+    public String resetPassword(String email) {
 
         // To apply the default app language instead of explicitly setting it.
         auth.useAppLanguage();
 
-        auth.sendPasswordResetEmail(email)
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        if (task.isSuccessful()) message[0] = "SUCCESS";
-                        else message[0] = "FAILED";
-                    }
-                });
+        auth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful())
+                    message[0] = "SUCCESS";
+                else
+                    message[0] = "FAILED";
+            }
+        });
         return message[0];
     }
 
-    public String signIn(String email, String password){
+    public String signIn(String email, String password) {
         auth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) message[0] = "SUCCESS";
+                        if (task.isSuccessful())
+                            message[0] = "SUCCESS";
                     }
                 }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                if (e instanceof FirebaseAuthInvalidCredentialsException)
-                    message[0] = "ERROR_INVALID_PASSWORD";
-                else if (e instanceof FirebaseAuthInvalidUserException) {
-					
-                    String errorCode =
-                            ((FirebaseAuthInvalidUserException) e).getErrorCode();
-                    message[0] = errorCode;
-                }
-            }
-        });
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        if (e instanceof FirebaseAuthInvalidCredentialsException)
+                            message[0] = "ERROR_INVALID_PASSWORD";
+                        else if (e instanceof FirebaseAuthInvalidUserException) {
+
+                            String errorCode = ((FirebaseAuthInvalidUserException) e).getErrorCode();
+                            message[0] = errorCode;
+                        }
+                    }
+                });
 
         return message[0];
     }
 
-    public String signUp(String email, String password){
+    public String signUp(String email, String password) {
         auth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) message[0] = "SUCCESS";
+                        if (task.isSuccessful())
+                            message[0] = "SUCCESS";
                     }
                 }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                if (e instanceof FirebaseAuthInvalidUserException) {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        if (e instanceof FirebaseAuthInvalidUserException) {
 
-                    String errorCode =
-                            ((FirebaseAuthInvalidUserException) e).getErrorCode();
-                    message[0] = errorCode;
-                }else if (e instanceof FirebaseAuthUserCollisionException ) {
-					
-                    String errorCode =
-                            ((FirebaseAuthUserCollisionException ) e).getErrorCode();
-                    message[0] = errorCode;
-				}else if (e instanceof FirebaseAuthWeakPasswordException)
-                    message[0] = "ERROR_WEAK_PASSWORD";
-            }
-        });
+                            String errorCode = ((FirebaseAuthInvalidUserException) e).getErrorCode();
+                            message[0] = errorCode;
+                        } else if (e instanceof FirebaseAuthUserCollisionException) {
+
+                            String errorCode = ((FirebaseAuthUserCollisionException) e).getErrorCode();
+                            message[0] = errorCode;
+                        } else if (e instanceof FirebaseAuthWeakPasswordException)
+                            message[0] = "ERROR_WEAK_PASSWORD";
+                    }
+                });
 
         return message[0];
     }
 
-    public void signOut(){
+    public void signOut() {
         auth.signOut();
     }
 
@@ -184,4 +186,3 @@ public class UserAuth {
     }
 
 }
-
