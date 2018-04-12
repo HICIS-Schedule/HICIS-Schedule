43c4c4bf8eed86be4df4865a7c47583a90b82218
Majed Sief Al-Nasr <magedelmasry@outlook.com>
2018-04-12 16:16:54 +0200

    MODIFIED Specialization.java enum
    Signed-off-by: Majed Sief Al-Nasr <magedelmasry@outlook.com>

--- app/src/main/java/csc/colleguescheduller/Models/Schedule/Schema/Specialization.java
+++ app/src/main/java/csc/colleguescheduller/Models/Schedule/Schema/Specialization.java
@@ -1,10 +1,5 @@
 package csc.colleguescheduller.Models.Schedule.Schema;
 
-/*
-Amr Samir
- */
-
-
 public enum Specialization {
-    General, Computer_Science, Information_Systems
+    GENERAL, CS, IS
 }
